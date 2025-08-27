77abb15d0986802a301aeb17a67b8272b3e0ca61
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-08-26 21:02:15 -0400

    AdrianLayme/Creacion Carpetas Models y Data hasta la clase Context

---
+++ EntityFrameworkSQLserver/Data/TareaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EntityFrameworkSQLserver.Models;
+
+namespace EntityFrameworkSQLserver.Data
+{
+    public class TareaDbContext : DbContext
+    {
+        public TareaDbContext(DbContextOptions<TareaDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+
+        protected TareaDbContext()
+        {
+
+        }
+    }
+}
---
+++ EntityFrameworkSQLserver/Models/Tarea.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityFrameworkSQLserver.Models
+{
+    public class Tarea
+    {
+        public int idTarea { get; set; }
+        public string nombreTarea { get; set; }
+        public DateTime fechaVencimientoTarea { get; set; }
+        public string estadoTarea { get; set; } 
+        public int idUsuario { get; set; }
+
+    }
+}
